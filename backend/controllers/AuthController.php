48ac09f42a054b237e6403c5c1b9acd2f3b18101
s.ischanova <s.ischanova@thecoders.ru>
2021-07-04 01:15:47 +0500

    авторизация

---
+++ backend/controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace backend\controllers;
+
+
+use common\models\LoginForm;
+use yii\web\Controller;
+
+class AuthController extends Controller
+{
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $this->layout = 'blank';
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        } else {
+            $model->password = '';
+
+            return $this->render('login', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return string
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+}
--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -87,37 +87,7 @@ class SiteController extends Controller
      *
      * @return string
      */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
 
-        $this->layout = 'blank';
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        } else {
-            $model->password = '';
-
-            return $this->render('login', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Logout action.
-     *
-     * @return string
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
 
     public function actionView($id)
     {
