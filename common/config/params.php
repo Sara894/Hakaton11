8b738691670071911b8135f7f6ecef283e17c6e7
s.ischanova <s.ischanova@thecoders.ru>
2021-07-04 17:46:19 +0500

    статистика по пользователям настройки

--- common/config/params.php
+++ common/config/params.php
@@ -6,4 +6,16 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
+    'statistics' => [
+
+        'days_default' => 3, //кол-во дней для вывода статистики по-умолчанию (сегодня/вчера/...)
+
+        'password' => 'klisl', //пароль для входа на страницу статистики. Если false (без кавычек) - вход без пароля
+
+        'authentication' => false, //если true, то статистика доступна только аутентифицированным пользователям
+
+        'auth_route' => 'site/login', //контроллер/действие для страницы аутентификации (по-умолчанию 'site/login')
+
+        'date_old' => 90 //удалять данные через х дней
+    ]
 ];
