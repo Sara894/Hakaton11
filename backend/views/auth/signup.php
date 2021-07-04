a98b75677dd8dd5f0212b52fbfdc6430c6640eea
s.ischanova <s.ischanova@thecoders.ru>
2021-07-04 11:43:33 +0500

    регистрация

---
+++ backend/views/auth/signup.php
@@ -0,0 +1,37 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+
+$this->title = 'Вход';
+?>
+<div class="leave-comment mr0"><!--leave comment-->
+    <div class="row">
+        <img src="/backend/web/public/images/logo.png" alt="">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="site-login">
+                <h1><?= Html::encode($this->title) ?></h1>
+
+                <p>Пожалуйста, заполните поля, чтобы зарегестрироваться:</p>
+
+                <?php $form = ActiveForm::begin(['id' => 'signup-form']); ?>
+
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+                <?= $form->field($model, 'password')->passwordInput() ?>
+
+
+                <div class="form-group">
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
+                </div>
+
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
+    </div>
+</div>
