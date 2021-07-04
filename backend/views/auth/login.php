4b15844b24b8c02a3af8f0cce3a5ab9187b91699
s.ischanova <s.ischanova@thecoders.ru>
2021-07-04 13:59:07 +0500

    изменение формы авторизации

--- backend/views/auth/login.php
+++ backend/views/auth/login.php
@@ -7,9 +7,11 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
+use yii\helpers\Url;
 
 $this->title = 'Вход';
 ?>
+
 <div class="leave-comment mr0"><!--leave comment-->
     <div class="row">
         <img src="/backend/web/public/images/logo.png" alt="">
@@ -22,14 +24,17 @@ $this->title = 'Вход';
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
                 <?= $form->field($model, 'password')->passwordInput() ?>
 
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('Войти', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
                 </div>
+                <h4>   <a href="<?= Url::toRoute(['auth/signup'])?>">Зарегестрироваться</a></h4>
+
 
             <?php ActiveForm::end(); ?>
 </div>
